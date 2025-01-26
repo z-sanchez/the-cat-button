badb79435181aaaa301c49babc1c8f7a72be73be
Ziek <zieksanchez3@gmail.com>
2025-01-25 19:07:39 -0800

    Update icons and icon configurations in manifest

--- vite.config.ts
+++ vite.config.ts
@@ -31,25 +31,11 @@ export default defineConfig({
             src: "/icons/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icons/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
           },
           {
             src: "/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icons/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
           },
         ],
         screenshots: [
