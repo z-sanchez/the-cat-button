a8bbe71b4a3416e9b33f644eb958fc6acecf4895
Ziek <zieksanchez3@gmail.com>
2025-02-18 15:38:37 -0800

    UI edits

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,13 @@ export const HomePage = () => {
     <GenericPage>
       <div className="w-full h-full flex justify-end items-center flex-col">
         <div className="flex w-full items-center rounded-md shadow-lg flex-col my-auto">
-          <div className="w-[400px] h-[250px]">
+          <div className="w-[400px] h-[200px]">
             <img
               src="https://plus.unsplash.com/premium_photo-1677101221533-52b45823a2dc?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="w-full h-full object-contain object-center rounded-md"
             />
           </div>
-          <div className="px-2 py-2 mx-2 mt-4 bg-gray-50 rounded-md">
+          <div className="px-2 py-2 mx-2 mt-4 bg-gray-50 rounded-md text-sm">
             <p className="text-2xl font-semibold text-primary">
               Sir Whiskerbottom the Third 🐱🎩
             </p>
@@ -22,7 +22,7 @@ export const HomePage = () => {
             <p>Origin: Meowsterdam</p>
             <div className="mt-2 bg-white rounded-md px-4 py-2">
               <p className="text-lg font-semibold text-black">Backstory</p>
-              <p className="text-gray-700">
+              <p className="text-gray-700 text-sm">
                 Sir Whiskerbottom III was once the esteemed feline of an
                 eccentric scholar who lived in a towering library. Over the
                 years, he developed a taste for fine literature (mostly sleeping
